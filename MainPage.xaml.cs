ca29df2540f28cce2080341416469424364f59f5
simon.mardine <simon.mardine@fiducial.net>
2023-12-13 12:06:09 +0100

    upload source code

---
+++ MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace MauiApp4
+{
+    public partial class MainVM : ObservableObject
+    {
+        [ObservableProperty]
+        ObservableCollection<string> liste;
+
+        public MainVM()
+        {
+            Liste = new ObservableCollection<string>
+                        {
+                            "AZ","ER","ty"
+                        };
+
+        }
+    }
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainVM();
+
+        }
+
+       
+    }
+
+}
